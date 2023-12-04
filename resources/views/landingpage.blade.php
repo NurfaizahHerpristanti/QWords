ba8a23df4f204950b1ce584692a71a8e1a2da1ac
NurfaizahHerpristanti <92023535+NurfaizahHerpristanti@users.noreply.github.com>
2023-12-04 22:26:04 +0700

    Initial commit

---
+++ resources/views/landingpage.blade.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<link rel = "stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="landingpage.css">
+</head>
+<body>
+    {{-- <img src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/logo-qw-light.webp" alt=""> --}}
+        {{-- <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+          Cloud Hosting
+        </button> --}}
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <img src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/logo-qw-light.webp" alt="">
+            <div class="container-fluid">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Cloud Hosting
+                  </a>
+                  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Server
+                  </a>
+                  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Domain
+                  </a>
+                  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Email Suite
+                  </a>
+                  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Services
+                  </a>
+                  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Internet Access
+                  </a>
+                  <h6>Promo</h6>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    
+                  </li>
+                  <li class="nav-item">
+                    
+                  </li>
+                  <li class="nav-item dropdown">
+                    
+                    <ul class="dropdown-menu">
+                      <li><a class="dropdown-item" href="#">Action</a></li>
+                      <li><a class="dropdown-item" href="#">Another action</a></li>
+                      <li><hr class="dropdown-divider"></li>
+                      <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                  </li>
+                  <li class="nav-item">
+                   
+                  </li>
+                </ul>
+                <form class="d-flex" role="search">
+                  <button class="btn btn-outline-success" type="submit">Log In</button>
+                </form>
+              </div>
+            </div>
+          </nav>
+
+        
+        <h1 class="Ppp">Cloud Hosting</h1>
+        <h1 class="Jjj">untuk</h1>
+        <h1 class="Kkk">Kesuksesan Websitemu!</h1> <br>
+        <p class="Lll">Qwords menawarkan layanan Cloud Hosting yang Cepat, Aman, dan Dukungan 24 jam untuk memenuhi kebutuhan hosting Anda. Tersertifikasi ISO 27001 dengan Data Center Tier 3.</p>
+        <br>
+        <h2 class="Mmm">#QwordsYourSuccess</h2>
+        <br><br>
+          <div class="button2">
+            <button type="button" class="btn" style="color: white; font-weight: bold">Pesan Sekarang</button></div>
+        {{-- <button class="btn-outline Aaa">Pesan Sekarang</button> --}}
+
+        
+        <section id="new-section">
+          <h2>Cari Nama Domainmu</h2>
+          <p>Nama Domain digunakan untuk mempermudah pengunjung menuju ke situs Anda. Pilih nama domain yang paling mencerminkan bisnis, produk, atau layanan Anda.</p>
+          <div class="input-group flex-nowrap">
+            <span class="input-group-text" id="addon-wrapping"></span>
+            <input type="text" class="form-control" placeholder="Cari nama domain anda" aria-label="Cari nama domain anda" aria-describedby="addon-wrapping">
+          </div>
+        </section>
+        
+        <div class="dropdown">
+          <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            .id
+          </button>
+        </div>
+
+</body>
+</html>
